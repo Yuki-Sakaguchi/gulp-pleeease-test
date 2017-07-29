52a1920f68f0690f051ca198a09e76715411d27b
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2017-07-30 00:43:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# sassを使うけど、ベンダープレフィックスは「Autoprefixer」をglupから使う
+
+
+## Autoprefixerとは
+cssにベンダープレフィックスを自動でつけるツール
+
+### 参考
+http://phiary.me/gulp-pleeease/
+http://kojika17.com/2014/01/autoprefixer.html
+http://parashuto.com/rriver/tools/using-custom-data-for-autoprefixer
+http://koltatt.net/web_design/gulp_pleeease/
+
+シェアの確認
+http://smatabinfo.jp/os/android/index.html
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var
+gulp     = require('gulp'),
+sass     = require('gulp-sass'), // sassのコンパイル
+bourbon  = require('node-bourbon'), // sassのbourbonを使う
+bitters  = require('bourbon-bitters'), // bourbonのbasecss
+pleeease = require('gulp-pleeease'), // cssのベンダープレフィックスの自動追加など
+plumber  = require('gulp-plumber'); // タスク中のエラーでwatchを止めない
+
+
+/**
+ * sass
+ */
+gulp.task('sass', function() {
+    // コンパイル元
+    gulp.src('./src/scss/**/*.scss')
+
+    // エラーでwatchを止めない
+    .pipe(plumber({
+        errorHandler: function(err) {
+            console.log(err.messageFormatted);
+            return this.emit('end');
+        }
+    }))
+
+    // コンパイル
+    .pipe(sass({
+        includePaths: [
+            bourbon.includePaths,
+            bitters.includePaths
+        ],
+        outputStyle : 'expanded'
+    }))
+
+    // コンパイル後のcssにベンダープレフィックスをつけたり、ミニファイしたりする
+    .pipe(pleeease({
+        // ベンダープレフィックスを追加する境界を設定
+        autoprefixer: {
+            browsers: [
+                "last 2 versions", // 主要ブラウザは最新のバージョンから２つ前まで
+                'ie >= 9', // IEは9以上
+                "iOS >= 8", // iOSは8以上
+                "Android >= 4.2", // Androidは4.2以上
+            ]
+        },
+        out         : "main.min.css", // 出力のファイル名を決められる
+        minifier    : false, // ミニファイフラグ
+    }))
+
+    // コンパイル先
+    .pipe(gulp.dest('./public/css/'));
+});
+
+
+/**
+ * watch
+ */
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
